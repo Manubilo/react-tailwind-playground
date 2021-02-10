13c45087ff677390f7606f034af6dde91aee5a2e
Manuel Delgado <mjdelgado@pucp.edu.pe>
2021-02-09 23:48:17 -0500

    finished landing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import React, { useState } from 'react';
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="relative bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto">
@@ -15,10 +16,218 @@ function App() {
           >
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
+
           <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-            <Navbar />
+            <nav
+              class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+              aria-label="Global"
+            >
+              <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                <div class="flex items-center justify-between w-full md:w-auto">
+                  <a href="/">
+                    <span className="sr-only">Workflow</span>
+                    <img
+                      alt="logo"
+                      className="h-8 w-auto sm:h-10"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                    />
+                  </a>
+                  {/* Modal Toggle */}
+                  <div className="-mr-2 flex items-center md:hidden">
+                    <button
+                      type="button"
+                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none
+              focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                      id="main-menu"
+                      aria-haspopup="true"
+                      onClick={() => setModalOpen(!modalOpen)}
+                    >
+                      <span className="sr-only">Open Main Menu</span>
+                      <svg
+                        class="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                <a
+                  href="/"
+                  className="font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Product
+                </a>
+                <a
+                  href="/"
+                  className="font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Features
+                </a>
+                <a
+                  href="/"
+                  className="font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="/"
+                  className="font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Company
+                </a>
+                <a
+                  href="/"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Log in
+                </a>
+              </div>
+            </nav>
           </div>
+
+          {/* Modal */}
+          <div
+            className={
+              'absolute top-0 inset-x-0 p-2 transition duration-150 ease-out transform origin-top-right md:hidden ' +
+              (modalOpen ? '' : 'hidden')
+            }
+          >
+            <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+              <div className="px-5 pt-4 flex items-center justify-between">
+                <div>
+                  <img
+                    alt="logo"
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <button
+                    onClick={() => setModalOpen(!modalOpen)}
+                    type="button"
+                    className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100
+              focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  >
+                    <span className="sr-only">Close main menu</span>
+                    <svg
+                      class="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="main-menu"
+              >
+                <div className="px-2 pt-2 pb-3 space-y-1" role="none">
+                  <a
+                    href="/"
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Product
+                  </a>
+                  <a
+                    href="/"
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Features
+                  </a>
+                  <a
+                    href="/"
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Marketplace
+                  </a>
+                  <a
+                    href="/"
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                  >
+                    Company
+                  </a>
+                </div>
+                <div role="none">
+                  <a
+                    href="/"
+                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Data to enrich your </span>
+                <span className="block text-indigo-600 xl:inline">
+                  online business
+                </span>
+              </h1>
+              <p className=" mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+                asperiores reprehenderit corrupti commodi magni tenetur facere?
+                Doloremque, atque? Aspernatur debitis necessitatibus iure iusto,
+                saepe cupiditate voluptatem labore ullam mollitia voluptas?
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <a
+                    href="/"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  >
+                    Get Started
+                  </a>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                  <a
+                    href="/"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </main>
         </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          alt=""
+        />
       </div>
     </div>
   );
